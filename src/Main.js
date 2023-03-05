867d25a4d87805b33dbe07b38d8775e36cd3ccaf
HarutyunyanLiana08 <harutyunyanliana08@gmail.com>
2023-03-05 20:28:32 +0400

    db

--- src/Main.js
+++ src/Main.js
@@ -1,29 +1,34 @@
-import chokerSharakan from  './img/choker-sharakan.png'
-import earrings from "./img/earrings.png"
-import handTriangle from "./img/hand-triangle.png"
-import necklace from "./img/necklace.png"
-import necklaceSrvak from "./img/necklace-srvak.png"
-import ring from "./img/ring.png"
+// import chokerSharakan from  './img/choker-sharakan.png'
+// import earrings from "./img/earrings.png"
+// import handTriangle from "./img/hand-triangle.png"
+// import necklace from "./img/necklace.png"
+// import necklaceSrvak from "./img/necklace-srvak.png"
+// import ring from "./img/ring.png"
 import { Link } from "react-router-dom"
-
+import {useEffect,useState} from 'react'
 export default function Main({handleClick}) {
   
-
-  const data = [
-    { id: 0, img: chokerSharakan, name: "Choker Sharakan", price: "149.000 AMD" },
-    { id: 1, img: earrings, name: "Earrings", price: "49.000 AMD" },
-    { id: 2, img: handTriangle, name: "Hand Triangle", price: "30.000 AMD" },
-    { id: 3, img: necklace, name: "Archangel Gabriel", price: "85.000 AMD" },
-    { id: 4, img: necklaceSrvak, name: "Necklace Srvak", price: "119.000 AMD" },
-    { id: 5, img: ring, name: "Ring Ag", price: "43.000 AMD" }
-  ];
+const [jewelry,setJewelry] = useState([]);
+useEffect(()=>{
+  fetch("http://localhost:3001")
+  .then(res=>res.json())
+  .then(res=>setJewelry(res));
+},[])
+  // const data = [
+  //   { id: 0, img: chokerSharakan, name: "Choker Sharakan", price: "149.000 AMD" },
+  //   { id: 1, img: earrings, name: "Earrings", price: "49.000 AMD" },
+  //   { id: 2, img: handTriangle, name: "Hand Triangle", price: "30.000 AMD" },
+  //   { id: 3, img: necklace, name: "Archangel Gabriel", price: "85.000 AMD" },
+  //   { id: 4, img: necklaceSrvak, name: "Necklace Srvak", price: "119.000 AMD" },
+  //   { id: 5, img: ring, name: "Ring Ag", price: "43.000 AMD" }
+  // ];
  
 
   return (
 
     <div className="container">
       
-      {data.map((item) => (
+      {jewelry.map((item) => (
         <div key={item.id} className="item">
           <Link to={`/product/${item.id}`}>
             <img src={item.img} alt={item.name} className="image" />
--- src/Product.js
+++ src/Product.js
@@ -5,71 +5,31 @@ import handTriangle from "./img/hand-triangle.png"
 import necklace from "./img/necklace.png"
 import necklaceSrvak from "./img/necklace-srvak.png"
 import ring from "./img/ring.png"
+import {useEffect,useState} from 'react'
 
-const data = [
-  {
-    id: 0,
-    img: chokerSharakan,
-    name: 'Choker Sharakan',
-    price: '149.000 AMD',
-    description: `This multi-strand pearl choker is as elegant as it is bold. Classic with a modern twist, Sharakan matches the timelessness of the pearls with the uniqueness of the vintage-inspired silver buckle. `,
-    weight:"25 gr (silver)",
-    stone: 'shell pearl 925 silver' 
-  },
-  {
-    id: 1,
-    img: earrings,
-    name: 'Earrings',
-    price: '49.000 AMD',
-    description: "Hethum I ruled the Armenian Kingdom of Cilicia from 1226 to 1270. These ethnic silver coin earrings are minted with a pattern which matches that of the currency used in the kingdom at the time.",
-    weight: "25gr (silver)",
-    stone: "pearl"
-  
-  },
-  {
-    id: 2,
-    img: handTriangle,
-    name: 'Hand Triangle',
-    price: '30.000 AMD',
-    description: 'This is the Hand Triangle description',
-  },
-  {
-    id: 3,
-    img: necklace,
-    name: 'Archangel Gabriel',
-    price: '85.000 AMD',
-    description: 'Archangel Gabriel is depicted on this silver necklace. In Christian prophecy, Gabriel was the messenger who appeared before the Virgin Mary and gave her news of being chosen to give birth to Christ.',
-    weight:"24gr",
-  },
-  {
-    id: 4,
-    img: necklaceSrvak,
-    name: 'Necklace Srvak',
-    price: '119.000 AMD',
-    description: 'This is the Necklace Srvak description',
-  },
-  {
-    id: 5,
-    img: ring,
-    name: 'Ring Ag',
-    price: '43.000 AMD',
-    description: 'This is the Ring Ag description',
-  },
-];
+
+
+
+
 
 export default function Product({handleClick}) {
   const { id } = useParams();
-  const product = data[id]
-
+  // const product = data[id]
+  const [jewelry,setJewelry] = useState([]);
+  useEffect(()=>{
+    fetch("http://localhost:3001/jewelry/"+id)
+    .then(res=>res.json())
+    .then(res=>setJewelry(res));
+  },[])
   return (
     <div className="more_info">
-      <img src={data[id].img} alt="" className="images" />
+      <img src={jewelry.img} alt="" className="images" />
       <div className="info">
-        <h2>{data[id].name}</h2>
-        <p className="des">{product.description}</p>
-        <h3>{data[id].price}</h3>
-        <h4> Weight:{data[id].weight}</h4>
-        <h4> Stone: {product.stone}</h4>
+        <h2>{jewelry.name}</h2>
+        <p className="des">{jewelry.description}</p>
+        <h3>{jewelry.price}</h3>
+        {/* <h4> Weight:{jewelry.weight}</h4> */}
+        {/* <h4> Stone: {product.stone}</h4> */}
         <button onClick={handleClick}>Add to cart </button>
        
       </div>
@@ -79,6 +39,48 @@ export default function Product({handleClick}) {
 
 
 
+// const data = [
+//   {
+//     id: 0,
+//     img: chokerSharakan,
+//     name: 'Choker Sharakan',
+//     price: '149.000 AMD',
+//     description: `This multi-strand pearl choker is as elegant as it is bold. Classic with a modern twist, Sharakan matches the timelessness of the pearls with the uniqueness of the vintage-inspired silver buckle. `,
+//     weight:"25 gr (silver)",
+//     stone: 'shell pearl 925 silver' 
+//   },
+//   {
+//     id: 1,
+//     img: earrings,
+//     name: 'Earrings',
+//     price: '49.000 AMD',
+//     description: "Hethum I ruled the Armenian Kingdom of Cilicia from 1226 to 1270. These ethnic silver coin earrings are minted with a pattern which matches that of the currency used in the kingdom at the time.",
+//     weight: "25gr (silver)",
+//     stone: "pearl"
+  
+//   },
+//   {
+//     id: 2,
+//     img: handTriangle,
+//     name: 'Hand Triangle',
+//     price: '30.000 AMD',
+//     description: 'This is the Hand Triangle description',
+//   },
+//   {
+//     id: 3,
+//     img: necklace,
+//     name: 'Archangel Gabriel',
+//     price: '85.000 AMD',
+//     description: 'Archangel Gabriel is depicted on this silver necklace. In Christian prophecy, Gabriel was the messenger who appeared before the Virgin Mary and gave her news of being chosen to give birth to Christ.',
+//     weight:"24gr",
+//   },
+//   {
+//     id: 4,
+//     img: necklaceSrvak,
+//     name: 'Necklace Srvak',
+//     price: '119.000 AMD',
+//     description: 'This is the Necklace Srvak description',
+//
 
 
 
